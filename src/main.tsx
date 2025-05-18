d3ea9a8d637f4c8291c51942a5c8ed292fa58633
Владимир Кирпичев <vkirpichev@inno.tech>
2025-05-18 22:23:08 +0300

    chore: init commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@mantine/core/styles.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { createTheme, MantineProvider } from "@mantine/core";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
+  </StrictMode>
+);
